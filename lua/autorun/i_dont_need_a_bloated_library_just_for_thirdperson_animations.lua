f3d9f7ed9815db38653dc8218cb0e78b4d505ad9
worldspawn <worldspawn@riseup.net>
2025-07-15 19:53:28 -0600

    yoink this so i can still use contextual animations

---
+++ lua/autorun/i_dont_need_a_bloated_library_just_for_thirdperson_animations.lua
@@ -0,0 +1,59 @@
+if DRC ~= nil then return end
+
+DRC = {}
+
+local GESTURE_TAG = "DRCNetworkGesture"
+
+if SERVER then
+	util.AddNetworkString(GESTURE_TAG)
+end
+
+function DRC:CallGesture(ply, slot, act, akill, fallback)
+	if not SERVER then return end
+	if not act then return end
+	if not IsValid(ply) then return end
+	if not slot or slot == "" then slot = GESTURE_SLOT_CUSTOM end
+	if not akill or akill == "" then akill = true end
+
+	if act == -1 then return end
+	if ply:SelectWeightedSequence(act) == -1 then act = fallback end
+	if fallback and ply:SelectWeightedSequence(act) == -1 then return end
+	if act == nil or act == -1 then return end -- what if someone wanted to use ACT_RESET, that fails `if not act`
+
+	if not ply:IsPlayer() then
+		timer.Simple(engine.TickInterval(), function()
+			if ply.RestartGesture then
+				ply:RestartGesture(act, true, akill)
+			end
+		end)
+	end
+
+	net.Start(GESTURE_TAG)
+	net.WriteEntity(ply)
+	-- why does the original base use floats???
+	net.WriteUInt(slot, 3)
+	net.WriteUInt(act, 11)
+	net.WriteBool(akill)
+	net.SendPVS(ply:EyePos())
+end
+
+if CLIENT then
+	net.Receive(GESTURE_TAG, function(len)
+		local ply = net.ReadEntity()
+		local slot = net.ReadUInt(3)
+		local act = net.ReadUInt(11)
+		local akill = net.ReadBool()
+
+		DRC:PlayGesture(ply, slot, act, akill)
+	end)
+
+	function DRC:PlayGesture(ply, slot, act, akill)
+		if IsValid(ply) and ply:IsPlayer() then
+			timer.Simple(engine.TickInterval(), function()
+				if IsValid(ply) then
+					ply:AnimRestartGesture(slot, act, akill)
+				end
+			end)
+		end
+	end
+end
