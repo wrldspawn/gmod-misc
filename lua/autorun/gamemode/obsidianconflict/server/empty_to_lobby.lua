501a6499243bd7af7eb63514f34c68304a14492e
worldspawn <worldspawn@riseup.net>
2025-09-20 23:41:02 -0600

    next frame

--- lua/autorun/gamemode/obsidianconflict/server/empty_to_lobby.lua
+++ lua/autorun/gamemode/obsidianconflict/server/empty_to_lobby.lua
@@ -2,17 +2,19 @@ local TAG = "oc_empty_to_lobby"
 
 gameevent.Listen("player_disconnect")
 hook.Add("player_disconnect", TAG, function()
-	local count = #player.GetHumans()
-	if count == 0 and not Obsidian.IsLobbyMap() then
-		print("Returning to lobby in 5 minutes")
+	timer.Simple(0, function()
+		local count = #player.GetHumans()
+		if count == 0 and not Obsidian.IsLobbyMap() then
+			print("Returning to lobby in 5 minutes")
 
-		timer.Create(TAG, 300, 1, function()
-			local self = GAMEMODE
-			self.LevelChangeTimer = self:CountdownDisplay("Level Change", 10)
-			self.BackToLobby = true
-			self.BackToLobbyTimer = CurTime()
-		end)
-	end
+			timer.Create(TAG, 300, 1, function()
+				local self = GAMEMODE
+				self.LevelChangeTimer = self:CountdownDisplay("Level Change", 10)
+				self.BackToLobby = true
+				self.BackToLobbyTimer = CurTime()
+			end)
+		end
+	end)
 end)
 
 gameevent.Listen("player_connect")
