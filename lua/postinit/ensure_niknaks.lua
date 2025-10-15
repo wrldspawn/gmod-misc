bd859df1dd8a30366f89ed835a1e5a8a7ea046b3
worldspawn <worldspawn@riseup.net>
2025-10-14 19:02:08 -0600

    ensure

--- lua/postinit/ensure_niknaks.lua
+++ lua/postinit/ensure_niknaks.lua
@@ -1,3 +1,5 @@
 -- the AddCSLuaFile call is in the module and so if the server doesn't load it, clients can't use it
--- (needed for showclips and eventually showtriggers)
+-- (needed for showclips, showtriggers, picker3)
 require("niknaks")
+require("picktext")
+
