eb61f83174cfc40b579e0b7498f0943b9440d82e
worldspawn <worldspawn@riseup.net>
2025-10-26 00:31:45 -0600

    hands/viewmodel stuff

--- lua/autorun/client/hands_fix.lua
+++ lua/autorun/client/hands_fix.lua
@@ -19,4 +19,27 @@ end)
 
 hook.Add("PreDrawPlayerHands", "hands_shadow_fix", function(hands, vm, ply, wep)
 	hands:DestroyShadow()
+	local target = LocalPlayer():GetObserverTarget()
+	if target:IsValid() then
+		hands:SetModel(target:GetHands():GetModel())
+		hands:SetColor(target:GetPlayerColor():ToColor())
+	end
+end)
+
+hook.Add("PreDrawViewModels", "holylib_viewmodel_fix", function()
+	local lply = LocalPlayer()
+	local target = lply:GetObserverTarget()
+	local in_eye = lply:GetObserverMode() == OBS_MODE_IN_EYE
+
+	for _, ply in player.Iterator() do
+		if ply == LocalPlayer() and not (target:IsValid() and not in_eye) then continue end
+
+		local shouldHide = true
+		if ply == target and in_eye then
+			shouldHide = false
+		end
+
+		local vm = ply:GetViewModel()
+		vm:SetNoDraw(shouldHide)
+	end
 end)
